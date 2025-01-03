1823794ff051209c08bba5b0c8e2d27d164d38bc
Gokul652003 <gokulkr652003@gmail.com>
2025-01-03 21:10:36 +0530

    refactor: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { DashBoard } from "./dashboard/DashBoard";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { DashBoard } from './dashboard/DashBoard';
+import { Transactions } from './transactions/Transaction';
+import { Header } from './page-components/Header';
 
 const App = () => (
   <Router>
-    <DashBoard />
+    <div className="w-screen h-screen bg-bg">
+      <div className="px-24">
+        <Header />
+      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/transaction" element={<Transactions />} />
+      </Routes>
+    </div>
   </Router>
 );
 
