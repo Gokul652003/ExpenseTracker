c3c20e68a092cd32b43ae4c49a50ab0790eed46b
Chithra Anil <chithralekha.anil004@gmail.com>
2025-01-14 21:43:35 +0530

    feat: categorycomponent

---
+++ src/react-components/Category/CategoryComponent.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CategoryComponent } from './CategoryComponent';
+
+const meta = {
+  component: CategoryComponent,
+} satisfies Meta<typeof CategoryComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args:{
+    color:"#8631E1",
+    label:"Personal"
+  }
+};
---
+++ src/react-components/Category/CategoryComponent.tsx
@@ -0,0 +1,22 @@
+import categoryIcon from './assets/CategoryIcon.svg'
+
+interface CategoryComponentProps{
+    color?:string;
+    label?:string;
+}
+export const CategoryComponent = ({color="grey",label="Food"}:CategoryComponentProps) => {
+  return (
+    <div >
+      <div className='flex gap-1.5'>
+        <img src={categoryIcon} />
+        <div className='rounded-lg p-2 flex gap-1.5 items-center'>
+            <div className='rounded w-3.5 h-3.5' style={{backgroundColor:color}}></div>
+            <div className='font-normal text-secondary text-base'>{label}</div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+
