8a0f6a5895c04ce83da9479c7711f46fd063411a
Chithra Anil <chithralekha.anil004@gmail.com>
2025-01-14 21:47:47 +0530

    resolve: format

--- src/react-components/Category/CategoryComponent.stories.tsx
+++ src/react-components/Category/CategoryComponent.stories.tsx
@@ -11,8 +11,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args:{
-    color:"#8631E1",
-    label:"Personal"
-  }
+  args: {
+    color: '#8631E1',
+    label: 'Personal',
+  },
 };
--- src/react-components/Category/CategoryComponent.tsx
+++ src/react-components/Category/CategoryComponent.tsx
@@ -1,22 +1,25 @@
-import categoryIcon from './assets/CategoryIcon.svg'
+import categoryIcon from './assets/CategoryIcon.svg';
 
-interface CategoryComponentProps{
-    color?:string;
-    label?:string;
+interface CategoryComponentProps {
+  color?: string;
+  label?: string;
 }
-export const CategoryComponent = ({color="grey",label="Food"}:CategoryComponentProps) => {
+export const CategoryComponent = ({
+  color = 'grey',
+  label = 'Food',
+}: CategoryComponentProps) => {
   return (
-    <div >
-      <div className='flex gap-1.5'>
+    <div>
+      <div className="flex gap-1.5">
         <img src={categoryIcon} />
-        <div className='rounded-lg p-2 flex gap-1.5 items-center'>
-            <div className='rounded w-3.5 h-3.5' style={{backgroundColor:color}}></div>
-            <div className='font-normal text-secondary text-base'>{label}</div>
+        <div className="rounded-lg p-2 flex gap-1.5 items-center">
+          <div
+            className="rounded w-3.5 h-3.5"
+            style={{ backgroundColor: color }}
+          ></div>
+          <div className="font-normal text-secondary text-base">{label}</div>
         </div>
       </div>
     </div>
-
-  )
-}
-
-
+  );
+};
