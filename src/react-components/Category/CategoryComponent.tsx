12eac8f427dea4dabd7b150c524be7e44739f83c
Chithra Anil <chithralekha.anil004@gmail.com>
2025-01-16 20:58:56 +0530

    feat: category component color picker

--- src/react-components/Category/CategoryComponent.tsx
+++ src/react-components/Category/CategoryComponent.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import categoryIcon from './assets/CategoryIcon.svg';
+import closeIcon from './assets/closeIcon.svg';
 
 interface CategoryComponentProps {
   color?: string;
@@ -8,17 +10,27 @@ export const CategoryComponent = ({
   color = 'grey',
   label = 'Food',
 }: CategoryComponentProps) => {
+  const [inputColor,setInputColor]=useState("rgba(169, 169, 169, 1)");
+  const [isContentEditable,setIsContentEditable]=useState(false);
+  console.log(isContentEditable)
   return (
-    <div>
-      <div className="flex gap-1.5">
-        <img src={categoryIcon} />
+    <div className="flex justify-between items-center">
+      <div className="flex gap-1.5 items-center">
+        <div>
+          <img src={categoryIcon} />
+        </div>
         <div className="rounded-lg p-2 flex gap-1.5 items-center">
           <div
             className="rounded w-3.5 h-3.5"
-            style={{ backgroundColor: color }}
-          ></div>
-          <div className="font-normal text-secondary text-base">{label}</div>
+            style={{ backgroundColor: inputColor }}
+          ><input type='color' className="rounded w-3.5 h-3.5 opacity-0 block" onChange={(e)=>{setInputColor(e.target.value)}}/></div>
+          <div className="flex ">
+            <div className="font-normal text-secondary text-base" contentEditable={isContentEditable} onClick={()=>setIsContentEditable(!isContentEditable)} >{label}</div>
+          </div>
         </div>
+      </div>
+      <div>
+        <img src={closeIcon} />
       </div>
     </div>
   );
