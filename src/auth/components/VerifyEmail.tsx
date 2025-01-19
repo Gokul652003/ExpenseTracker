bcad74cbc6a1efa1b06b8a168a0a0185a32f8ce3
Chithra Anil <chithralekha.anil004@gmail.com>
2025-01-19 17:55:38 +0530

    resolve : built error in pipeline

--- src/auth/components/VerifyEmail.tsx
+++ src/auth/components/VerifyEmail.tsx
@@ -1,28 +1,26 @@
 import expenseTrackerLogo from '@/assets/expense-tracker-logo.svg';
 export const VerifyEmail = ({ email }: { email: string }) => {
   return (
-    <div>
-      <div className="flex flex-col justify-center h-screen gap-7 px-48">
-        <div>
-          <img src={expenseTrackerLogo} />
-        </div>
-        <div className="flex flex-col gap-1">
-          <span className="font-normal text-[32px] text-textColor">
-            Verify your email address
+    <div className="flex flex-col justify-center h-screen gap-7 px-48">
+      <div>
+        <img src={expenseTrackerLogo} />
+      </div>
+      <div className="flex flex-col gap-1">
+        <span className="font-normal text-[32px] text-textColor">
+          Verify your email address
+        </span>
+        <span className="font-normal text-base text-secondary">
+          We have sent a verification link to your email address
+          <span className="text-textColor"> {email}</span>. Please check your
+          inbox and click the link to verify your account.
+        </span>
+      </div>
+      <div>
+        <button className="bg-primary py-3 px-10 rounded-full">
+          <span className="font-normal text-base text-textColor">
+            Resent Email
           </span>
-          <span className="font-normal text-base text-secondary">
-            We have sent a verification link to your email address
-            <span className="text-textColor"> {email}</span>. Please check your
-            inbox and click the link to verify your account.
-          </span>
-        </div>
-        <div>
-          <button className="bg-primary py-3 px-10 rounded-full">
-            <span className="font-normal text-base text-textColor">
-              Resent Email
-            </span>
-          </button>
-        </div>
+        </button>
       </div>
     </div>
   );
