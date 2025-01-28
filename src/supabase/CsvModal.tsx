9cb180a15cde28773859885b50795ee60c9d2b14
Gokul652003 <gokulkr652003@gmail.com>
2025-01-28 17:26:14 +0530

    feat: implement userId with csv

--- src/supabase/CsvModal.tsx
+++ src/supabase/CsvModal.tsx
@@ -48,7 +48,7 @@ const CsvUploaderComponent = ({
           className="mt-4 w-full px-4 py-2 border border-gray-300 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         {uploading && <p className="mt-2 text-gray-500">Uploading...</p>}
-        {message && <p className="mt-2 text-gray-500">{message}</p>}
+        {message && <p className="mt-2 text-red-500">{message}</p>}
       </div>
     </Modal>
   );
